ef56a260b99f435e4993219c3dc03efc3aa3dc01
CarbonylGroup <carbonylgp@gmail.com>
2021-12-10 13:41:09 -0500

    Generate top level screenshot list

--- generateCompressedScreenshots.sh
+++ generateCompressedScreenshots.sh
@@ -1,11 +1,14 @@
-ROOT=$(pwd)
 SCREENSHOT_OUTPUT_DIR="./screenshots"
 SCREENSHOT_COMPRESSED_OUTPUT_DIR="./screenshots-compressed"
 
 rm -r $SCREENSHOT_COMPRESSED_OUTPUT_DIR
 cp -r $SCREENSHOT_OUTPUT_DIR $SCREENSHOT_COMPRESSED_OUTPUT_DIR
 
-for device in "$SCREENSHOT_COMPRESSED_OUTPUT_DIR"/*/; do
+cd "$SCREENSHOT_COMPRESSED_OUTPUT_DIR"
+ROOT=$(pwd)
+printf "{" > images.json
+for device in */; do
+    printf "\"$(echo $device | sed 's/.$//')\":" >> images.json
     cd "$device"
     DEVICE=$(pwd)
     for language in */; do
@@ -17,10 +20,23 @@ for device in "$SCREENSHOT_COMPRESSED_OUTPUT_DIR"/*/; do
         done
         rm *.json
         raw=$(ls *.png | jq -R -s -c 'split("\n")[:-1]')
-        echo "$raw" > images.json
+        printf "$raw" > images.json
         cd ..
     done
+    printf "{" > images.json
+    for language in */; do
+        printf "\"$(echo $language | sed 's/.$//')\":" >> images.json
+        cat "$language/images.json" >> images.json
+        printf "," >> images.json
+    done
+    sed -i '' -e '$ s/.$//' images.json
+    printf "}" >> images.json
     cd $ROOT
+
+    cat "$device/images.json" >> images.json
+    printf "," >> images.json
 done
+sed -i '' -e '$ s/.$//' images.json
+printf "}" >> images.json
 
 
