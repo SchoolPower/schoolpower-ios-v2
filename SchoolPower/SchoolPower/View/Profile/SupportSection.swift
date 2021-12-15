d7de6bf90c7c18057608c660d40f0543b2f2688d
CarbonylGroup <carbonylgp@gmail.com>
2021-12-15 18:54:53 -0500

    Break support section

--- SchoolPower/SchoolPower/View/Profile/SupportSection.swift
+++ SchoolPower/SchoolPower/View/Profile/SupportSection.swift
@@ -9,33 +9,37 @@ import SwiftUI
 
 struct SupportSection: View {
     var body: some View {
-        Section() {
-            SendBugReportEmail {
-                SettingItem(
-                    title: "Report Bugs",
-                    description: "Contact us via Email"
-                )
+        Group {
+            Section() {
+                SendBugReportEmail {
+                    SettingItem(
+                        title: "Report Bugs",
+                        description: "Contact us via Email"
+                    )
+                }
+                Button(action: {
+                    UIApplication.shared.open(URL(string: Constants.supportUsURL)!)
+                }) {
+                    SettingItem(
+                        title: "Support Us"
+                    )
+                }
             }
-            Button(action: {
-                UIApplication.shared.open(URL(string: Constants.supportUsURL)!)
-            }) {
-                SettingItem(
-                    title: "Support Us"
-                )
-            }
-            Button(action: {
-                UIApplication.shared.open(URL(string: Constants.websiteURL)!)
-            }) {
-                SettingItem(
-                    title: "Website"
-                )
-            }
-            Button(action: {
-                UIApplication.shared.open(URL(string: Constants.sourceCodeURL)!)
-            }) {
-                SettingItem(
-                    title: "Source Code"
-                )
+            Section() {
+                Button(action: {
+                    UIApplication.shared.open(URL(string: Constants.websiteURL)!)
+                }) {
+                    SettingItem(
+                        title: "Website"
+                    )
+                }
+                Button(action: {
+                    UIApplication.shared.open(URL(string: Constants.sourceCodeURL)!)
+                }) {
+                    SettingItem(
+                        title: "Source Code"
+                    )
+                }
             }
         }
     }
